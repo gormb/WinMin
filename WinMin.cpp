64c83fab521f6e267dceff4cbdb409624b21b332
Gorm Braarvig <gorm.braarvig@digdir.no>
2023-11-21 10:03:28 +0100

    qwe

--- WinMin.cpp
+++ WinMin.cpp
@@ -1,28 +1,24 @@
 #include <windows.h>
+
 // release x86
 // debug x64
 
-#define _i __int64
-#ifdef NDEBUG
-#define _i __int32
-#pragma comment(linker, "/ENTRY:aWinMain")
-#pragma comment(linker, "/MERGE:.text=.rdata") 
-#endif
-
-_i CALLBACK WinProc(HWND hDlg, UINT msg, WPARAM wP, LPARAM) {
+int _t_WinProc(HWND hDlg, UINT msg, WPARAM wP, LPARAM) {
     if (msg == WM_COMMAND)
         if (wP < 3) EndDialog(hDlg, 0);
     return (msg == WM_INITDIALOG);
 }
 
 #ifdef NDEBUG
-int APIENTRY aWinMain() {
-    return DialogBoxParamA(0, (char*)103, 0, WinProc, 0);
-}
-#else
-__int32 APIENTRY wWinMain(HINSTANCE, HINSTANCE, LPWSTR, int) {
-    DialogBox(0, (wchar_t*)103, 0, WinProc);
-    return 0;
-}
+#pragma comment(linker, "/ENTRY:aWinMain")
+#pragma comment(linker, "/MERGE:.text=.rdata") 
 
-#endif // NDEBUG
+__int32 CALLBACK aWinProc(HWND hDlg, UINT msg, WPARAM wP, LPARAM) { return _t_WinProc(hDlg, msg, wP, 0); }
+int APIENTRY aWinMain() { return DialogBoxParamA(0, (char*)103, 0, aWinProc, 0); }
+
+#else
+
+__int64 CALLBACK wWinProc(HWND hDlg, UINT msg, WPARAM wP, LPARAM) { return _t_WinProc(hDlg, msg, wP, 0); }
+__int32 APIENTRY wWinMain(HINSTANCE, HINSTANCE, LPWSTR, int) { return (__int32)DialogBoxParamW(0, (wchar_t*)103, 0, wWinProc, 0); }
+
+#endif
